6100a09ead61495bbd50f7340157053bee813235
Adam Saulnier <adam.saulnier@fourwindsinteractive.com>
2018-11-01 10:22:55 -0600

    update pyg

--- pig-latin.py
+++ pig-latin.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 pyg = 'ay'
-=======
-pyg = 'fourwinds'
->>>>>>> Demo
 
 original = input('Enter a word:') #WORD
 
@@ -13,8 +9,4 @@ if len(original) > 0 and original.isalpha():  # adding alpha char
   new_word = new_word[1:len(new_word)] #ordway
   print (new_word)
 else:
-<<<<<<< HEAD
     print ('input can only contain alpha letters')
-=======
-    print ('needs to be alpha characters')
->>>>>>> Demo
